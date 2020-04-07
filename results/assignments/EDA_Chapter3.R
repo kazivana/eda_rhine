5ad48fe2d71c12bb0652cacb67a63153cac368f9
kazivana <kazivana@gmail.com>
2020-04-07 14:11:08 +0200

    Chapter 3, second commit

--- results/assignments/EDA_Chapter3.R
+++ results/assignments/EDA_Chapter3.R
@@ -46,3 +46,38 @@ ggplot(data = runoff_stations_lls, aes(x = lon, y = lat, color = 'green', label=
   geom_point() + geom_text(aes(label=sname), hjust=0, vjust=0) + theme(legend.position = 'right')
 
 
+# 3. 
+
+runoff_day <- readRDS('./data/runoff_day_raw.rds')
+head(runoff_day)
+
+station_time <- runoff_day[, .(start = min(year(date)),
+                               end = max(year(date))),
+                           by = sname]
+table(station_time$end)
+
+
+## Explorer questions
+
+# 1a: Units for area: m^2
+# 1b: Units for runoff: m^3/s
+
+#2: avg. catchment area and runoff
+
+avg_catchment <- mean(runoff_stations_aas$area)
+avg_catchment
+# 67069.5 m^2
+
+avg_runoff <- mean(runoff_day$value)
+avg_runoff
+# 2028.642 m^3
+
+#3: avg. runoff per station
+
+head(runoff_day)
+
+runoff_day[, list(avg=mean(value)), by=sname]
+
+#4: They have a somewhat inverse relationship. Higher altitudes would
+# generally indicate 'peaks', hills or mountains, which by definition are
+# not wide enough to have a large area the way plains at lower altitudes can.
