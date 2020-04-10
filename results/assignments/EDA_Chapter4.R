fa29740767f2c5224a2742d95d99e01ad24ba05b
kazivana <kazivana@gmail.com>
2020-04-10 13:37:49 +0200

    Chapter 4, final commit

--- results/assignments/EDA_Chapter4.R
+++ results/assignments/EDA_Chapter4.R
@@ -1,218 +1,79 @@
 library(data.table)
 library(ggplot2)
 library(mapview) 
-library(sf) 
+library(sf)
+library(moments)
 
-# Load data
-
-colset_4 <- c("#D35C37", "#BF9A77", "#D6C6B9", "#97B8C2")
-
-runoff_stations <- readRDS('data/runoff_stations.rds')
-runoff_day <- readRDS('data/runoff_day.rds')
-
-# Summary stats
-
-runoff_stats <- runoff_day[, .(mean_day = round(mean(value), 0),
-                               sd_day = round(sd(value), 0),
-                               min_day = round(min(value), 0),
-                               max_day = round(max(value), 0)), by = sname]
-head(runoff_stats, 4)
-
-ggplot(runoff_day, aes(value)) +
-  geom_histogram(fill = '#97B8C2') +
-  facet_wrap(~sname, scales = 'free') +
-  theme_bw()
-
-# Discretization
-
-runoff_stats_class <- runoff_stats[, .(sname,
-                                       mean_day)]
-runoff_stats_class[, runoff_class := factor('low')]
-runoff_stats_class[mean_day >= 1000 & mean_day < 2000, runoff_class := factor('medium')]
-runoff_stats_class[mean_day >= 2000, runoff_class := factor('high')]
-
-runoff_stations[, area_class := factor('small')]
-runoff_stations[area >= 1000 & area < 130000, area_class := factor('medium')]
-runoff_stations[area >= 130000, area_class := factor('large')]
-
-runoff_stations[, alt_class := factor('low')]
-runoff_stations[altitude >= 50 & altitude < 400, alt_class := factor('medium')]
-runoff_stations[altitude >= 400, alt_class := factor('high')]
-
-to_merge <- runoff_stats_class[, .(sname, runoff_class)]
-runoff_summary <- runoff_stations[to_merge, on = 'sname']
-
-# Aggregation
-
-runoff_day[, year := year(date)]
-runoff_day[, month := month(date)]
-
-runoff_month <- runoff_day[, .(value = mean(value)), by = .(month, year, sname)]
-runoff_month[, date := as.Date(paste0(year, '-', month, '-1'))]
-
-ggplot(runoff_month, aes(x = factor(month), y = value)) +
-  geom_boxplot(fill = colset_4[4]) +
-  facet_wrap(~sname, scales = 'free') +
-  theme_bw()
-
-runoff_year <- runoff_day[, .(value = mean(value)), by = .(year, sname)]
-
-ggplot(runoff_year[year > 1980], aes(x = year, y = value)) +
-  geom_line(col = colset_4[1])+
-  geom_point(col = colset_4[1]) +
-  facet_wrap(~sname, scales = 'free') +
-  theme_minimal()
-
-runoff_day[month == 12 | month == 1 | month == 2, season :='winter']
-runoff_day[month == 3 | month == 4 | month == 5, season := 'spring']
-runoff_day[month == 6 | month == 7 | month == 8, season := 'summer']
-runoff_day[month == 9 | month == 10 | month == 11, season := 'autumn']
-runoff_day[, season := factor(season, levels = c('winter', 'spring', 'summer', 'autumn'))]
-
-runoff_year[, value_norm := (value - mean(value)) / sd(value), by = sname]
+runoff_summary <- readRDS('./data/runoff_summary.rds')
+runoff_stats <- readRDS('./data/runoff_stats.rds')
+runoff_day <- readRDS('./data/runoff_day.rds')
+runoff_month <- readRDS('./data/runoff_month.rds')
+runoff_summer <- readRDS('./data/runoff_summer.rds')
+runoff_winter <- readRDS('./data/runoff_winter.rds')
+runoff_year <- readRDS('./data/runoff_year.rds')
+runoff_stations <- readRDS('./data/runoff_stations.rds')
 n_stations <- nrow(runoff_summary)
-
-ggplot(runoff_year[year > 1980], aes(x = year, y = value_norm, col = sname)) +
-  geom_line() +
-  geom_point() +
-  scale_color_manual(values = colorRampPalette(colset_4)(n_stations))+
-  theme_bw()
-
-runoff_winter <- runoff_day[season == 'winter',
-                            .(value = mean(value)),
-                            by = .(sname, year)]
-runoff_summer <- runoff_day[season == 'summer',
-                            .(value = mean(value)),
-                            by = .(sname, year)]
-
-
-# Map
-
-station_coords_sf <- st_as_sf(runoff_summary, 
-                              coords = c('lon', 'lat'),
-                              crs = 4326)
-mapview(station_coords_sf, map.types = 'Stamen.TerrainBackground')
-
-runoff_summary <- runoff_summary[order(-altitude)]
-runoff_summary <- runoff_summary[c(1:15, 17:16)]
-runoff_summary <- cbind(order_id = 1:17, runoff_summary)
-runoff_summary[, sname := reorder(sname, order_id)]
-
-runoff_day[, sname := factor(sname, levels = runoff_summary$sname)]
-runoff_month[, sname := factor(sname, levels = runoff_summary$sname)]
-runoff_summer[, sname := factor(sname, level = runoff_summary$sname)]
-runoff_winter[, sname := factor(sname, levels = runoff_summary$sname)]
-runoff_year[, sname := factor(sname, levels = runoff_summary$sname)]
-
-# Data consistency
-
-dt <-  runoff_summary[, .(sname, area, alt_class)]
-to_plot <- runoff_stats[dt, on = 'sname']
-
-ggplot(to_plot, aes(x = mean_day, y = area, col = sname, cex = alt_class)) +
-  geom_point() +
-  scale_color_manual(values = colorRampPalette(colset_4)(n_stations)) +
-  theme_bw()
+colset_4 <- c("#D35C37", "#BF9A77", "#D6C6B9", "#97B8C2")
 
 # Navigator tasks ---------------
 
 #1: 
 
-typeof(runoff_stats)
+median_runoff <- runoff_day[, .(median = median(value)), sname]
+runoff_stats <- median_runoff[runoff_stats, on = 'sname']
+runoff_stats_tidy <- melt(runoff_stats, id.vars = 'sname')
+to_plot <- runoff_stats_tidy[variable != 'sd_day']
 
-ggplot(to_plot, aes(x = sd_day, y = area, col = sname, cex = alt_class)) +
-  geom_point() +
-  scale_color_manual(values = colorRampPalette(colset_4[4])(n_stations))+
-  theme_bw()
+ggplot(to_plot, aes(x = sname, y = value, size = 0.5)) +
+  geom_point(aes(col = variable, shape = variable))
 
-ggplot(to_plot, aes(x = min_day, y = area, col = sname, cex = alt_class)) +
-  geom_point() +
-  scale_color_manual(values = colorRampPalette(colset_4[1])(n_stations))+
-  theme_bw()
-
-ggplot(to_plot, aes(x = max_day, y = area, col = sname, cex = alt_class)) +
-  geom_point() +
-  scale_color_manual(values = colorRampPalette(colset_4[2])(n_stations))+
-  theme_bw()
 
 #2a:
 
-skew_def <- length(runoff_day$value)/6.79
-skew_def
-
-COV_def <- sd(runoff_day$value)/mean(runoff_day$value)
-COV_def
+runoff_day_skew <- runoff_day[, .(skew = moments::skewness(value)), by = sname]
+runoff_stats <- runoff_day_skew[runoff_stats, on = 'sname']
+runoff_stats[, cv_day := sd_day / mean_day]
 
 
-runoff_stats[, skew := length(mean_day)/6.79, by = sname]
-runoff_stats
+runoff_skew_cv <- runoff_stats[, c('sname', 'skew', 'cv_day')]
 
-runoff_stats[, cov := sd_day/mean_day, by = sname]
-runoff_stats
+runoff_skew_cv
 
-#2b: 
-
-skew_cov_dt <- data.table(runoff_stats$skew, runoff_stats$cov)
-skew_cov_dt
-
-skew_cov_dt[, .(skew = V1), (cov = V2)]
 
 #3:
 
-ggplot(to_plot, aes(x = sname, y = max_day, group=sname))+
-  geom_boxplot(fill='blue', alpha=0.2)+
-  xlab('station')
+to_merge <- runoff_summary[, .(sname, runoff_class)]
+to_plot <- runoff_month[to_merge, on = 'sname']
 
-ggplot(to_plot, aes(x = sname, y = mean_day, group=sname))+
-  geom_boxplot(fill='red', alpha=0.2)+
-  xlab('station')
+ggplot(to_plot, aes(x = factor(month), y = value, fill = runoff_class)) +
+  geom_boxplot() +
+  facet_wrap(~sname, scales = 'free') +
+  theme_bw()
 
-ggplot(to_plot, aes(x = sname, y = min_day, group=sname))+
-  geom_boxplot(fill='green', alpha=0.2)+
-  xlab('station')
 
-ggplot(to_plot, aes(x = sname, y = sd_day, group=sname))+
-  geom_boxplot(fill='black', alpha=0.2)+
-  xlab('station')
-
-#4:
+#5:
 
 #area
-runoff_stations[, area_class := factor('small')]
-runoff_stations[area >= 10000 & area < 100000, area_class := factor('medium')]
-runoff_stations[area >= 100000, area_class := factor('large')]
+runoff_summary[, area_class := factor('small')]
+runoff_summary[area >= 10000 & area < 100000, area_class := factor('medium')]
+runoff_summary[area >= 100000, area_class := factor('large')]
 
 #altitude
-runoff_stations[, alt_class := factor('low')]
-runoff_stations[altitude >= 100 & altitude < 500, alt_class := factor('medium')]
-runoff_stations[altitude >= 500, alt_class := factor('high')]
+runoff_summary[, alt_class := factor('low')]
+runoff_summary[altitude >= 100 & altitude < 500, alt_class := factor('medium')]
+runoff_summary[altitude >= 500, alt_class := factor('high')]
 
-ggplot(to_plot, aes(x = mean_day, y = area, col = sname, cex = alt_class )) +
-  geom_point() +
-  scale_color_manual(values = colorRampPalette(colset_4)(n_stations)) +
-  theme_bw()
+runoff_summary$mean_day <- runoff_stats$mean_day
 
+ggplot(runoff_summary, aes(x = mean_day, y = area,
+                           col = area_class, cex = alt_class)) +
+  geom_point()
 
-dt2 <-  runoff_summary[, .(sname, altitude, area_class)]
-to_plot2 <- runoff_stats[dt2, on = 'sname']
-
-ggplot(to_plot2, aes(x = mean_day, y = altitude, col = sname, cex = area_class )) +
-  geom_point() +
-  scale_color_manual(values = colorRampPalette(colset_4)(n_stations)) +
-  theme_bw()
-
-
-ggplot(runoff_stats, aes(x = sname, y = mean_day, fill = mean_day)) +
-  geom_boxplot() +
-  geom_jitter(color='black', size=0.4, alpha=0.9) +
-  facet_wrap(~sname, scales = 'free')+
-  theme_bw()
 
 # the outliers for lowest values (Reki, Dier, Doma) are all in Switzerland. 
 # meanwhile the outliers for highest daily outflow are in the Netherlands and nearby
 # Germany, indicating since the catchment area is larger towards the end of the river
 # the outflow is also larger. 
-  
 
 
 # Explorer's questions ----------------------
@@ -230,58 +91,78 @@ runoff_stats[, quantile(mean_day)]
 # low. They are both surrounded by a lot of smaller water bodies as well, which collect
 # water from the main Rhine river, which might be a factor in explaining the high maximums.
 
-#4:
-runoff_stats
-runoff_month
+
+
+#4: 
+
+runoff_summer_TM1 <- runoff_summer[, min(value), by = sname]
+runoff_summer_TM2 <- runoff_summer[, max(value), by = sname]
+runoff_summer_to_merge <- merge(runoff_summer_TM1, runoff_summer_TM2, by = 'sname')
+colnames(runoff_summer_to_merge) <- c('sname', 'min', 'max')
+runoff_summer_to_merge
 runoff_summer
-runoff_winter
+runoff_summer_minmax <- merge(runoff_summer, runoff_summer_to_merge, by = 'sname')
+runoff_summer_minmax
+runoff_summer_max_final <- runoff_summer_minmax[runoff_summer_minmax$value == runoff_summer_minmax$max] 
+runoff_summer_min_final <- runoff_summer_minmax[runoff_summer_minmax$value == runoff_summer_minmax$min] 
 
+ggplot(data = runoff_summer_max_final, aes(x = sname, y = max, label = year)) +
+  geom_point() +
+  geom_text(aes(label = year), hjust = 0, vjust = 0) 
 
-max_runoff_stats <- runoff_day[, .(max_runoff=max(value)), by = sname]
-max_runoff_stats <- max_runoff_stats[, max_runoff := round(max_runoff, 2)]
-maxplot <- ggplot(max_runoff_stats, aes(x = sname, y = max_runoff)) +
-  geom_bar(stat = 'identity', width = 0.4)
-print(maxplot + labs(title = "Maximum runoff per station", x = 'Station', y = 'Max runoff (m³)'))
+ggplot(data = runoff_summer_min_final, aes(x = sname, y = min, label = year)) +
+  geom_point() +
+  geom_text(aes(label = year), hjust = 0, vjust = 0) 
 
+runoff_w_TM1 <- runoff_winter[, min(value), by = sname]
+runoff_w_TM2 <- runoff_winter[, max(value), by = sname]
+runoff_w_to_merge <- merge(runoff_w_TM1, runoff_w_TM2, by = 'sname')
+colnames(runoff_w_to_merge) <- c('sname', 'min', 'max')
 
-runoff_winter_max <- runoff_day[season == 'winter',
-                            .(value = max(value)),
-                            by = .(sname, year)]
-runoff_summer_max <- runoff_day[season == 'summer',
-                            .(value = max(value)),
-                            by = .(sname, year)]
-runoff_autumn_max <- runoff_day[season == 'autumn',
-                            .(value = max(value)),
-                            by = .(sname, year)]
-runoff_spring_max <- runoff_day[season == 'spring',
-                            .(value = max(value)),
-                            by = .(sname, year)]
+runoff_w_minmax <- merge(runoff_winter, runoff_w_to_merge, by = 'sname')
 
-sum(runoff_winter_max$value)
-sum(runoff_summer_max$value)
-sum(runoff_spring_max$value)
-sum(runoff_autumn_max$value)
+runoff_w_max_final <- runoff_w_minmax[runoff_w_minmax$value == runoff_w_minmax$max] 
+runoff_w_min_final <- runoff_w_minmax[runoff_w_minmax$value == runoff_w_minmax$min] 
 
-runoff_winter
+ggplot(data = runoff_w_max_final, aes(x = sname, y = max, label = year)) +
+  geom_point() +
+  geom_text(aes(label = year), hjust = 0, vjust = 0) 
 
+ggplot(data = runoff_w_min_final, aes(x = sname, y = min, label = year)) +
+  geom_point() +
+  geom_text(aes(label = year),hjust = 0, vjust = 0) 
 
-ggplot(runoff_winter, aes(value)) +
-  geom_boxplot(fill = "#97B8C2") +
-  facet_wrap(~sname, scales = 'free') +
-  theme_bw()
+runoff_m_TM1 <- runoff_month[,min(value),by = sname]
+runoff_m_TM2 <- runoff_month[,max(value),by = sname]
+runoff_m_to_merge <- merge(runoff_m_TM1, runoff_m_TM2, by = 'sname')
+colnames(runoff_m_to_merge) <- c('sname', 'min', 'max')
 
-ggplot(runoff_summer, aes(value)) +
-  geom_boxplot(fill = "#97B8C2") +
-  facet_wrap(~sname, scales = 'free') +
-  theme_bw()
+runoff_m_minmax <- merge(runoff_month, runoff_m_to_merge, by = 'sname')
 
-ggplot(runoff_spring_max, aes(value)) +
-  geom_boxplot(fill = "#97B8C2") +
-  facet_wrap(~sname, scales = 'free') +
-  theme_bw()
+runoff_m_max_final <- runoff_m_minmax[runoff_m_minmax$value == runoff_m_minmax$max] 
+runoff_m_min_final <- runoff_m_minmax[runoff_m_minmax$value == runoff_m_minmax$min] 
 
-ggplot(runoff_autumn_max, aes(value)) +
-  geom_boxplot(fill = "#97B8C2") +
-  facet_wrap(~sname, scales = 'free') +
-  theme_bw()
+ggplot(data = runoff_m_max_final, aes(x = sname, y = max, label = year)) +
+  geom_point() +
+  geom_text(aes(label = year), hjust = 0, vjust=0) 
 
+ggplot(data = runoff_m_min_final, aes(x = sname, y = min, label = year)) +
+  geom_point() +
+  geom_text(aes(label = year), hjust = 0, vjust=0) 
+
+runoff_y_TM1 <- runoff_year[, min(value), by = sname]
+runoff_y_TM2 <- runoff_year[, max(value), by = sname]
+runoff_y_to_merge <- merge(runoff_y_TM1, runoff_y_TM2, by = 'sname')
+colnames(runoff_y_to_merge) <- c('sname', 'min', 'max')
+
+runoff_y_minmax <- merge(runoff_year, runoff_y_to_merge, by = 'sname')
+
+runoff_y_max_final <- runoff_y_minmax[runoff_y_minmax$value == runoff_y_minmax$max] 
+runoff_y_min_final <- runoff_y_minmax[runoff_y_minmax$value == runoff_y_minmax$min] 
+ggplot(data = runoff_y_max_final, aes(x = sname, y = max, label = year)) +
+  geom_point() +
+  geom_text(aes(label = year), hjust = 0, vjust = 0) 
+
+ggplot(data = runoff_y_min_final, aes(x = sname, y = min, label = year)) +
+  geom_point() +
+  geom_text(aes(label = year), hjust = 0, vjust = 0) 
